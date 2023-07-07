8f693b9b167f69fd6c46e76eda97b9930735d32b
Yasi <113851370+YasiruBasura@users.noreply.github.com>
2023-07-07 21:32:45 +0530

    Service

--- src/Comments/Comments.js
+++ src/Comments/Comments.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Comments = () => {
+const Comments = ({currentUserId}) => {
   return (
     <div>This is the Comments</div>
   )
---
+++ src/Services/CommentService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const COMMENT_API_BASE_URL = "http://localhost:8080/api/v1"
+class CommentService{
+
+    saveComment(epic) { //take employee object as the input parameter
+    
+            return axios.post(COMMENT_API_BASE_URL,epic)  //here we are passing the entire object to the localhost url 
+    
+    
+    }
+
+    getComment(){
+        return axios.get(COMMENT_API_BASE_URL);
+    }
+
+    deleteComment(id){
+        return axios.delete(COMMENT_API_BASE_URL+ '/' +id);
+    }
+
+   
+
+    
+}
+
+export default new CommentService()
